6246ce127bd0f2334d36aeadf2430bb6ae98c309
jrossgatterer <j.rossgatterer@gmx.net>
2020-05-19 13:12:55 +0200

    Projekt erstellt

---
+++ app/src/main/java/com/example/posprojekt/Person.java
@@ -0,0 +1,20 @@
+package com.example.posprojekt;
+
+public class Person {
+
+    String vorname;
+    String nachname;
+    double guthaben;
+    String emailAdresse;
+    int telefonNr;
+
+
+    public Person(String vorname, String nachname, double guthaben, String emailAdresse, int telefonNr) {
+        this.vorname = vorname;
+        this.nachname = nachname;
+        this.guthaben = guthaben;
+        this.emailAdresse = emailAdresse;
+        this.telefonNr = telefonNr;
+    }
+
+}
